2346c281b91f28d868518cd1558bfe147f272439
Tristin <tmille2004@gmail.com>
2023-01-15 13:22:47 -0600

    creates documentation page

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import homepage
+from .views import homepage, documentation, dashboard
 
 urlpatterns = [
-    path('', homepage, name='homepage')
+    path('', homepage, name='homepage'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('documentation/', documentation, name='documentation')
 ]
--- main/views.py
+++ main/views.py
@@ -5,3 +5,9 @@ def homepage(request):
     print(request.user)
     
     return render(request, "homepage.html", context={})
+
+def dashboard(request):
+    return render(request, 'dashboard.html', context={})
+    
+def documentation(request):
+    return render(request, 'documentation.html', context={})
