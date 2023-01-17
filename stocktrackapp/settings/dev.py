531cea8a99af0edd94393f8427d61b2b2ffc6535
Tristin <tmille2004@gmail.com>
2023-01-16 22:35:22 -0600

    creates seperate settings files for dev and prod

---
+++ stocktrackapp/settings/dev.py
@@ -0,0 +1,12 @@
+from .base import *
+import os
+
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
---
+++ stocktrackapp/settings/prod.py
@@ -0,0 +1,7 @@
+from .base import *
+import os
+# setup postgres database
+
+DEBUG = False
+ALLOWED_HOSTS = []
+STATIC_ROOT = '/app/static/'
